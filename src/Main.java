575f441f40b8904d32234ea3b47a9c594740bd07
iriardelean <iriardelean33@gmail.com>
2025-10-21 14:56:58 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,19 @@ public class Main {
         int[] b2 = {5, 4, 0, 0, 0, 0, 0, 0, 0};
         System.out.println(Arrays.toString(a2) + " - " + Arrays.toString(b2));
         System.out.println("Ergebnis: " + Arrays.toString(ZahlRechner.differenz(a2, b2)));
+
+        // aufgabe 3
+        System.out.println("Aufgabe 3");
+        int[] a3 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        int b3 = 2;
+        System.out.println(Arrays.toString(a3) + " * " + b3);
+        System.out.println("Ergebnis: " + Arrays.toString(ZahlRechner.multiplikation(a3, b3)));
+
+        // aufgabe 4
+        System.out.println("Aufgabe 4");
+        int[] a4 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        int b4 = 2;
+        System.out.println(Arrays.toString(a4) + " / " + b4);
+        System.out.println("Ergebnis: " + Arrays.toString(ZahlRechner.division(a4, b4)));
     }
 }
--- src/ZahlRechner.java
+++ src/ZahlRechner.java
@@ -1,4 +1,7 @@
 public class ZahlRechner {
+
+    // Berechnen Sie die Summe. Die Zahlen haben die gleiche Anzahl an Ziffern.
+    // z.B. [1 3 0 0 0 0 0 0 0] + [8 7 0 0 0 0 0 0 0] = [1 0 0 0 0 0 0 0 0 0]
     public static int[] summe(int[] a, int[] b) {
         int n = a.length;
         int[] ergebnis = new int[n + 1];
@@ -14,6 +17,8 @@ public class ZahlRechner {
         return ergebnis;
     }
 
+    // Berechnen Sie die Differenz. Die Zahlen haben die gleiche Anzahl an Ziffern.
+    // z.B. [8 3 0 0 0 0 0 0 0] - [5 4 0 0 0 0 0 0 0] = [2 9 0 0 0 0 0 0 0]
     public static int[] differenz(int[] a, int[] b) {
         int n = a.length;
         int[] ergebnis = new int[n];
@@ -34,5 +39,62 @@ public class ZahlRechner {
             ergebnis[i] = differenz;
         }
         return ergebnis;
+    }
+
+    // Berechnen Sie die Multiplikation. Die erste Zahl ist eine große Zahl, die zweite Zahl ist nur eine Ziffer.
+    // z.B. [2 3 6 0 0 0 0 0 0] * 2 = [4 7 2 0 0 0 0 0 0]
+    public static int[] multiplikation(int[] a, int b) {
+        int n = a.length;
+        int[] ergebnis = new int[n+1];
+        int uebertrag = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int produkt = (a[i] * b) + uebertrag;
+            ergebnis[i + 1] = produkt % 10;
+            uebertrag = produkt / 10;
+        }
+
+        ergebnis[0] = uebertrag;
+
+        return trimmeFuehrendeNullen(ergebnis);
+    }
+
+    // hilfsfunktion
+    private static int[] trimmeFuehrendeNullen(int[] arr) {
+        int ersteNichtNull = 0;
+
+        while (ersteNichtNull < arr.length - 1 && arr[ersteNichtNull] == 0)
+            ersteNichtNull++;
+
+        if (ersteNichtNull == 0)
+            return arr;
+
+        int[] getrimmt = new int[arr.length - ersteNichtNull];
+
+        for (int i = 0; i < getrimmt.length; i++)
+            getrimmt[i] = arr[ersteNichtNull + i];
+
+        return getrimmt;
+    }
+
+    // Berechnen Sie die ganzzahlige Division. Die erste Zahl ist eine große Zahl, die zweite Zahl ist nur eine Ziffer.
+    // z.B. [2 3 6 0 0 0 0 0 0] / 2 = [1 1 8 0 0 0 0 0 0]
+    public static int[] division(int[] a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division durch Null ist nicht erlaubt.");
+        }
+
+        int n = a.length;
+        int[] quotient = new int[n];
+        int rest = 0;
+
+        for (int i = 0; i < n; i++) {
+            int dividend = rest * 10 + a[i];
+
+            quotient[i] = dividend / b;
+            rest = dividend % b;
+        }
+
+        return trimmeFuehrendeNullen(quotient);
     }
 }
